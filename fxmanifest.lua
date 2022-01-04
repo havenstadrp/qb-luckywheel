a9d023f93f2907860c90110affecc3efef89b310
ItsANoBrainer <itsanobrainergg@protonmail.com>
2022-01-04 01:10:40 -0500

    Adjusted manifest

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -6,8 +6,6 @@ author 'ItsANoBRainer'
 description 'Casino Lucky Wheel'
 version '1.0.0'
 
-this_is_a_map 'yes'
-
 shared_script 'config.lua'
 client_script 'client/main.lua'
 server_script 'server/main.lua'
