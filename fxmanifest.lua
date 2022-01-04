7682adec5e63f312b2ccc8cb77670a7c9360d121
ItsANoBrainer <itsanobrainergg@protonmail.com>
2022-01-04 01:07:01 -0500

    Fixed manifest

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,8 +1,12 @@
+-- https://docs.fivem.net/docs/scripting-reference/resource-manifest/resource-manifest/ --
 fx_version 'cerulean'
 game 'gta5'
 
-description 'QB-Luckywheel'
+author 'ItsANoBRainer'
+description 'Casino Lucky Wheel'
 version '1.0.0'
+
+this_is_a_map 'yes'
 
 shared_script 'config.lua'
 client_script 'client/main.lua'
