e4937beafae24c36c568b9764fc3bcf649728a2a
WeRBorg <55774910+WeRBorg@users.noreply.github.com>
2022-01-28 09:56:05 -0500

    Update config.lua

--- config.lua
+++ config.lua
@@ -6,7 +6,8 @@ Config.Amount = 25                                        -- Amount of chips nee
 Config.WheelPos = vector3(949.6968, 45.06157, 70.9)       -- Position for wheel
 Config.VehPos = vector4(935.11, 42.61, 71.525, 0.0)       -- Position for vehicle
 Config.UseThirdEyeInteraction = false
-Config.GiveCarOnWin = false
+Config.GiveCarOnWin = true
+Config.SpinCooldown = 120                                  -- Minutes
 
 -- CASINO_DIA_PL    - Falling Diamonds
 -- CASINO_HLW_PL    - Falling Skulls
