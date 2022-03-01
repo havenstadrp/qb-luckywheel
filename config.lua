60a9c67cda5d39f734875a7c9806d828804b1491
bedar89 <bedar89@gmail.com>
2022-03-01 18:17:32 +0000

    changed car to laferrari

--- config.lua
+++ config.lua
@@ -1,7 +1,7 @@
 Config = {}
 
 Config.WheelModel = `vw_prop_vw_luckywheel_02a`
-Config.Vehicle = `furia`                                  -- Model name for vehicle to win/on display
+Config.Vehicle = `laferrari`                                  -- Model name for vehicle to win/on display
 Config.Amount = 25                                        -- Amount of chips needed to spin wheel
 Config.WheelPos = vector3(949.6968, 45.06157, 70.9)       -- Position for wheel
 Config.VehPos = vector4(935.11, 42.61, 71.525, 0.0)       -- Position for vehicle
