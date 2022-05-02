8356753c36536f3e0f489a798c348562bbafedc1
Tony <97451137+TonybynMp4@users.noreply.github.com>
2022-05-02 05:49:31 +0200

    Update en.lua

--- locales/en.lua
+++ locales/en.lua
@@ -4,7 +4,7 @@ local Translations = {
         time = 'Please, come back later to try your Luck on our LuckyWheel!',
     },
     success = {
-        You_won = 'You Won %{prize}',
+        You_Won = 'You Won %{prize}',
     },
     general = {
         pistol = 'A .50 Pistol!',
@@ -14,7 +14,7 @@ local Translations = {
         sandwich_water = '....Sandwich and Water?',
         money = '$ %{money_amount} !',
         car = 'the car!',
-        spin_Wheel = 'Spin Wheel %{wheel_amount} Chips',
+        spin_wheel = 'Spin Wheel %{wheel_amount} Chips',
         sender = 'The Diamond Casino',
         subject = 'Your new car!',
         message = 'Your new car is waiting for you at the Motel Parking!',
